8831d91294e2b34b705dbe46eb070a82eccb76b7
jihopark <jihodayo@gmail.com>
2024-12-20 17:27:30 +0900

    Feat: 에러 및 스캘래톤 코드 작성

--- src/app/compoenets/NewsCard.tsx
+++ src/app/compoenets/NewsCard.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React, { useState, useEffect, Suspense } from "react";
+import React, { useState, useEffect } from "react";
 import { fetchNews } from "@/app/api/mainApi";
+import Skeleton from "./Skeleton";
+import Error from "./ErrorCompoenet";
 
 interface News {
   title: string;
@@ -12,8 +14,8 @@ interface News {
 
 const NewsCard = () => {
   const [news, setNews] = useState<News[]>([]);
-
   const [error, setError] = useState<unknown>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
@@ -22,46 +24,46 @@ const NewsCard = () => {
         setNews(result);
       } catch (error) {
         setError(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     getData();
   }, []);
 
-  if (error) return <div>Error: {String(error)}</div>;
+  if (loading) {
+    return <Skeleton />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
 
   return (
-    <Suspense
-      fallback={
-        <div className="flex justify-center items-center h-full">
-          <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
-        </div>
-      }
-    >
-      <div className="w-full h-fit bg-primary-300 text-primary-100 rounded-md p-4 md:p-6 lg:p-10 flex flex-col justify-between">
-        <h2 className="font-sansBold text-base md:text-xl lg:text-2xl">
-          최신 소식
-        </h2>
-        <div className="font-body mt-4 flex flex-col justify-between flex-grow">
-          {news.map((newsItem, index) => (
-            <div key={index}>
-              <p className="text-xs text-primary-200">
-                {newsItem.release.split("T")[0]}
-              </p>
-              <a
-                href={newsItem.url}
-                target="_blank"
-                rel="noreferrer"
-                className="text-sm truncate block w-full hover:underline"
-              >
-                {newsItem.title}
-              </a>
-              {index < 2 && <hr className="border-t border-primary-200 my-4" />}
-            </div>
-          ))}
-        </div>
+    <div className="w-full h-fit bg-primary-300 text-primary-100 rounded-md p-4 md:p-6 lg:p-10 flex flex-col justify-between">
+      <h2 className="font-sansBold text-base md:text-xl lg:text-2xl">
+        최신 소식
+      </h2>
+      <div className="font-body mt-4 flex flex-col justify-between flex-grow">
+        {news.map((newsItem, index) => (
+          <div key={index}>
+            <p className="text-xs text-primary-200">
+              {newsItem.release.split("T")[0]}
+            </p>
+            <a
+              href={newsItem.url}
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm truncate block w-full hover:underline"
+            >
+              {newsItem.title}
+            </a>
+            {index < 2 && <hr className="border-t border-primary-200 my-4" />}
+          </div>
+        ))}
       </div>
-    </Suspense>
+    </div>
   );
 };
 
