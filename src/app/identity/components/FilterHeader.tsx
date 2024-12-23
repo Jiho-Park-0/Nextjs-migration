504dd1d34cb093e23a6ee49e11b727e213974dbf
jihopark <jihodayo@gmail.com>
2024-12-23 20:25:08 +0900

    Feat: 필터 그릅 css설정
    #3

--- src/app/identity/components/FilterHeader.tsx
+++ src/app/identity/components/FilterHeader.tsx
@@ -5,7 +5,7 @@ import { Button } from "@material-tailwind/react";
 import { useResetRecoilState } from "recoil";
 import { optionsState } from "@/recoils/atoms";
 
-const FilterHeader: React.FC = () => {
+const FilterHeader = () => {
   const resetOptions = useResetRecoilState(optionsState);
 
   const handleResetOptions = () => {
@@ -14,17 +14,15 @@ const FilterHeader: React.FC = () => {
   };
 
   return (
-    <div className={`w-[300px] min-w-[300px] hidden lg:block mt-2`}>
-      <div className="flex justify-between items-center mb-8">
-        <span className="text-3xl lg:text-4xl">필터</span>
-        <Button
-          onClick={handleResetOptions}
-          className="bg-primary-400 px-4 py-1 text-lg text-primary-100 hover:bg-primary-300 rounded"
-          placeholder={undefined}
-        >
-          초기화
-        </Button>
-      </div>
+    <div className="flex justify-between items-center mb-8">
+      <span className="text-3xl lg:text-4xl">필터</span>
+      <Button
+        onClick={handleResetOptions}
+        className="bg-primary-400 px-4 py-1 text-lg text-primary-100 hover:bg-primary-300 rounded"
+        placeholder={undefined}
+      >
+        초기화
+      </Button>
     </div>
   );
 };
--- src/app/identity/page.tsx
+++ src/app/identity/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import FilterHeader from "./components/FilterHeader";
+import Filter from "./components/Filter";
 
 export default function IdentityPage() {
   return (
     <div className="flex font-sans text-primary-100 font-bold mt-4">
-      <FilterHeader />
+      <div className="w-[300px] min-w-[300px] hidden lg:block mt-2">
+        {" "}
+        <FilterHeader />
+        <Filter />
+      </div>
     </div>
   );
 }
