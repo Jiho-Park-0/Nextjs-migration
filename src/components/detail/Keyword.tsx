f46244eddc7f20d7a5ca80127192a6c514d21a22
jihopark <jihodayo@gmail.com>
2025-01-07 14:59:27 +0900

    Feat: 스킬 페이지 구현 #6

---
+++ src/components/detail/Keyword.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import keyword_data from "@/constants/keyword.json";
+import KeywordHighlighted from "@/components/detail/KeywordHighlighted";
+
+interface IdentityKeywordProps {
+  keywords: string[];
+}
+
+const IdentityKeyword = ({ keywords }: IdentityKeywordProps) => {
+  return (
+    <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+      {keywords.map((keyword, index) => {
+        const keywordInfo = keyword_data.find((item) => item.name === keyword);
+
+        if (keywordInfo && keywordInfo.content) {
+          const content = keywordInfo.content;
+          return (
+            <div key={index} className="p-4 rounded-md bg-primary-500">
+              <Image
+                src={`/assets/keyword/${keyword}.webp`}
+                alt="resourceImg"
+                className="inline-block h-[1.1em] mr-1 mb-[2px]"
+                width={1024}
+                height={1024}
+              />
+              <span>{keyword}</span>
+              <br />
+              <KeywordHighlighted text={content} />
+            </div>
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
+};
+
+export default IdentityKeyword;
---
+++ src/components/detail/identity/IdentitySkills.tsx
@@ -0,0 +1,75 @@
+import IdentitySkillCard from "./IdentitySkillCard";
+
+import useStore from "@/zustand/store";
+
+interface Props {
+  identitySkills: Skill[][];
+}
+
+interface Skill {
+  name: string;
+  power: string;
+  type: string;
+  resource: string;
+  skillSeq: number;
+  skillPower: number;
+  coinPower: number;
+  coinNum: number;
+  atkWeight: number;
+  level: number;
+  normalEffect: string;
+  coin1Effect: string;
+  coin2Effect: string;
+  coin3Effect: string;
+  coin4Effect: string;
+  coin5Effect: string;
+}
+
+const IdentitySkills = ({ identitySkills }: Props) => {
+  const synchronization = useStore((state) => state.synchronizationState);
+
+  // 배열을 크기가 2인 배열로 변환하는 함수
+  const splitIntoPairs = (skillsArray: Skill[][]): Skill[][] => {
+    const result: Skill[][] = [];
+    for (const skills of skillsArray) {
+      for (let i = 0; i < skills.length; i += 2) {
+        result.push(skills.slice(i, i + 2));
+      }
+    }
+    return result;
+  };
+
+  // 변환된 배열을 저장
+  const processedIdentitySkills = splitIntoPairs(identitySkills);
+
+  // skillSeq에 따라 type을 설정하는 함수
+  const getTypeBySkillSeq = (skillSeq: number) => {
+    switch (skillSeq) {
+      case 1:
+        return "1 Skill";
+      case 2:
+        return "2 Skill";
+      case 3:
+        return "3 Skill";
+      case 4:
+        return "DEFENSE";
+      default:
+        return "Unknown Skill";
+    }
+  };
+
+  return (
+    <div>
+      {processedIdentitySkills.map((skills, index) => (
+        <IdentitySkillCard
+          key={index}
+          type={getTypeBySkillSeq(skills[0]?.skillSeq || 0)}
+          synchronization={synchronization.synchronization}
+          skill={skills}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default IdentitySkills;
