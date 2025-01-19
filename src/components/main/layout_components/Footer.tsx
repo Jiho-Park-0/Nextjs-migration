abd8860ee42c106326e166c130e3be0a8aba7b4b
jihopark <jihodayo@gmail.com>
2025-01-19 15:27:00 +0900

    Feat: 버전 추가

--- src/components/main/layout_components/Footer.tsx
+++ src/components/main/layout_components/Footer.tsx
@@ -25,6 +25,9 @@ const Footer = () => {
             <br />
             <br />© 2024 baslimbus
             <br />
+            <br />
+            2025.01.19 Ver.NextJS
+            <br />
             단테의 빵과 수프는 프로젝트문 공식 서비스가 아니며, 모든 게임
             아트워크, 정보, 애셋의 권리와 저작권은 해당
           </Typography>
