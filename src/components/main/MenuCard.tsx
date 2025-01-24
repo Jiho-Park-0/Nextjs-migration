0feb7274df6fbfd6b1e1366f1dea5f96126595c6
jihopark <jihodayo@gmail.com>
2025-01-24 18:10:08 +0900

    Feat: 메뉴카드 롤백

--- src/components/main/MenuCard.tsx
+++ src/components/main/MenuCard.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 interface Menu {
   name: string;
@@ -16,17 +15,12 @@ const MenuCard = ({ menu }: { menu: Menu }) => {
       <Link href={menu.link}>
         <div className="relative w-full h-full group">
           {/* 배경 이미지 */}
-          <div className="absolute inset-0 rounded-lg">
-            <Image
-              src={menu.image}
-              alt={menu.name}
-              fill
-              className="object-cover transition-transform duration-500 ease-out transform group-hover:scale-110"
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              priority
-              quality={30}
-            />
-          </div>
+          <div
+            className="absolute inset-0 bg-cover bg-center rounded-lg transition-transform duration-500 ease-out transform group-hover:scale-110"
+            style={{
+              backgroundImage: `url(${menu.image})`,
+            }}
+          ></div>
 
           {/* 오버레이: 배경 이미지 위에 반투명한 레이어 */}
           <div className="absolute inset-0 bg-black bg-opacity-60 rounded-lg"></div>
