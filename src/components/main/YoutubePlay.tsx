3c79ea9217825b44940596686e4f6384f5faf264
jihopark <jihodayo@gmail.com>
2025-02-03 11:00:42 +0900

    feat: 발푸밤 스위치 추가

--- src/components/main/YoutubePlay.tsx
+++ src/components/main/YoutubePlay.tsx
@@ -1,50 +1,80 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { getYoutube } from "@/api/mainApi"; // YouTube API 호출 함수
+import { getYoutube } from "@/api/mainApi";
 import YouTube from "react-youtube";
 import { ApiError } from "@/interfaces/apiError";
 
 interface News {
   videoId: string;
 }
-
 const YoutubePlay = () => {
   const [data, setData] = useState<News[]>([]);
   const [error, setError] = useState<ApiError | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
         const data = await getYoutube();
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         if (data === undefined || data.length === 0) {
-          setData([{ videoId: "HTRQgFYCXHY" }]); // 기본값 설정
-          return;
+          setData([{ videoId: "HTRQgFYCXHY" }]);
+        } else {
+          setData(data);
         }
-
-        setData(data);
       } catch (err) {
         if (err instanceof Error) {
           setError({ message: err.message });
         } else {
           setError({ message: "An unknown error occurred" });
         }
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchNews();
   }, []);
 
-  console.log(data);
+  if (loading) {
+    return (
+      <div className="w-full mx-auto">
+        <div className="aspect-video bg-gray-800 animate-pulse"></div>
+      </div>
+    );
+  }
+
+  if (error || data.length === 0) {
+    return (
+      <div className="w-full mx-auto">
+        <div className="aspect-video ">
+          <YouTube
+            videoId={"HTRQgFYCXHY"}
+            className="aspect-video"
+            opts={{
+              width: "100%",
+              height: "100%",
+              playerVars: {
+                autoplay: 0,
+                controls: 1,
+                modestbranding: 1,
+                rel: 0,
+                showinfo: 0,
+              },
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="w-full mx-auto">
       <div className="aspect-video">
         <YouTube
-          videoId={
-            error || data.length === 0 ? "HTRQgFYCXHY" : data[0]?.videoId
-          } // data가 없는 경우 프로젝트문 대표 영상 출력 "HTRQgFYCXHY"
+          videoId={data[0]?.videoId || "HTRQgFYCXHY"}
           className="aspect-video"
           opts={{
             width: "100%",
