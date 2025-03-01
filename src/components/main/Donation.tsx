94843f99941acb41f334901582e5c9843e365b74
jihopark <jihodayo@gmail.com>
2025-03-01 21:29:05 +0900

    style: 도네 문구 수정

--- src/components/main/Donation.tsx
+++ src/components/main/Donation.tsx
@@ -5,8 +5,7 @@ const Donation = () => {
     <Link href="https://buymeacoffee.com/breadandsoup">
       <div className="w-full h-fit bg-yellow-800 text-primary-400 text-xs md:text-sm lg:text-base rounded-md p-2 md:p-4 lg:p-6  justify-between">
         {/* 빵, 수프만 볼드 */}
-        <span className="font-bold">빵🍞 </span>하고{" "}
-        <span className="font-bold">수프 🥣</span> 사주기
+        <span className="font-bold">빵🍞 </span> 많이 사주기
       </div>
     </Link>
   );
