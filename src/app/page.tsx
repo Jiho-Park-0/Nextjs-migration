1fa4d01c7544fd829b92c197b32f57a0d5772b15
jihopark <jihodayo@gmail.com>
2025-05-06 16:42:07 +0900

    docs: 공지 삭제, navbar추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const YoutubePlay = lazy(() => import("@/components/main/YoutubePlay"));
 export default function Home() {
   return (
     <div className="py-1 md:py-10 flex flex-col gap-3 md:gap-12">
-      <div className="text-white bg-primary-300 rounded-md p-2 md:p-4 lg:p-6 flex flex-col justify-between">
+      {/* <div className="text-white bg-primary-300 rounded-md p-2 md:p-4 lg:p-6 flex flex-col justify-between">
         <div className="text-center">
           안녕하세요. 단빵숲 입니다. <br />
           서버점검으로 인해 잠시 사이트가 비활성화 될 수 있습니다. 양해
@@ -28,7 +28,7 @@ export default function Home() {
           페이지에서 이전에 선택하신 인격들이 저장됩니다. <br />
           이용에 참고 바랍니다.
         </div>
-      </div>
+      </div> */}
       <div className="w-full mx-auto">
         <MainCarousel />
       </div>
