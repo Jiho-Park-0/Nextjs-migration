d32f31c39ace353493a84b4355d7dc1b97ef0df1
jihopark <jihodayo@gmail.com>
2025-06-15 16:50:19 +0900

    docs: 메인 화면 수정

--- src/components/main/ApplyForm.tsx
+++ src/components/main/ApplyForm.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 const ApplyForm = () => {
   return (
-    <Link href="https://docs.google.com/forms/d/1C0jJuaS8GgATmrvetARmOqRER6dx_MWx-YhnPUQ54Xg/edit">
-      <div className="w-full h-full bg-primary-100 text-primary-300 hover:text-primary-500 text-sm md:text-base lg:text-lg rounded-md p-2 md:p-4 lg:p-6 flex justify-center items-center">
-        <span className="font-bold">단빵숲 프론트엔드 지원!</span>
-      </div>
-    </Link>
+    <div className=" w-full h-full bg-primary-100 text-primary-300 hover:text-primary-500 text-sm md:text-base lg:text-lg rounded-md p-2 md:p-4 lg:p-6 flex flex-col justify-center items-center">
+      <span className="font-bold">프론트엔드 지원 마감</span>
+
+      <span>지원해주셔서 감사합니다!</span>
+    </div>
   );
 };
 
