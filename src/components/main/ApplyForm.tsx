b63450993d4b94a28ef649519f102cd54863cef6
jihopark <jihodayo@gmail.com>
2025-06-16 20:24:42 +0900

    docs: apply 수정

--- src/components/main/ApplyForm.tsx
+++ src/components/main/ApplyForm.tsx
@@ -2,10 +2,10 @@
 
 const ApplyForm = () => {
   return (
-    <div className=" w-full h-full bg-primary-100 text-primary-300 hover:text-primary-500 text-sm md:text-base lg:text-lg rounded-md p-2 md:p-4 lg:p-6 flex flex-col justify-center items-center">
-      <span className="font-bold">프론트엔드 지원 마감</span>
+    <div className=" w-full h-full bg-primary-300 text-primary-100  text-sm md:text-base lg:text-lg rounded-md p-2 md:p-4 lg:p-6 flex flex-col justify-center items-center">
+      <span className="font-bold">단빵숲 디자인에 관심있으신 분!</span>
 
-      <span>지원해주셔서 감사합니다!</span>
+      <span>bas.limbus@gmail.com ⬅️</span>
     </div>
   );
 };
